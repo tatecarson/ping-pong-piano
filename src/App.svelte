2e7264a0f4c1c599be21fe36f271aa1b4c7f9800
tatecarson <tatecarson@gmail.com>
2022-07-29 20:58:34 +0000

    working pretty well!

--- src/App.svelte
+++ src/App.svelte
@@ -25,12 +25,13 @@
 
   let chartRef;
 
-  let move_threshold = 1;
+  let move_threshold = 2;
   let move_forward_threshold = 0.1;
 
   // detect steps with p5 or not
   let detectStepsp5 = false;
 
+  let showButton = false;
   let permissionGranted = false;
 
   let lastDebounceTime = 0;
@@ -65,10 +66,12 @@
         DeviceOrientationEvent.requestPermission()
           .catch(() => {
             // show permission dialog only the first time
-            let button = p5.createButton("click to allow access to sensors");
-            button.style("font-size", "24px");
-            button.center();
-            button.mousePressed(requestAccess);
+            // let button = p5.createButton("click to allow access to sensors");
+            // button.style("font-size", "24px");
+            // button.center();
+            // button.mousePressed(requestAccess);
+
+            showButton = true;
             throw Error("try again");
           })
           .then((e) => {
@@ -83,23 +86,10 @@
       }
     };
 
-    function requestAccess() {
-      // @ts-ignore
-      DeviceOrientationEvent.requestPermission()
-        .then((response, event) => {
-          if (response == "granted") {
-            permissionGranted = true;
-          } else {
-            permissionGranted = false;
-          }
-        })
-        .catch(console.error);
-
-      this.remove();
-    }
-
     p5.draw = () => {
       if (!permissionGranted) return;
+
+      p5.background(255);
 
       // source: https://www.mathworks.com/help/matlabmobile_android/ug/counting-steps-by-capturing-acceleration-data.html
       let v = p5.createVector(
@@ -120,14 +110,38 @@
 
       let meanPeakHeight = std(magNoG);
 
-      // TODO: use this to figure out if the steps are going over
       data.yMarkers[0].value = meanPeakHeight + move_threshold;
 
-      // TODO: play with the threshold value here
       if (magNoG[49] > meanPeakHeight + move_threshold) {
         // console.log(`Step ${magNoG} ${meanPeakHeight}`);
         console.log("Step");
+        p5.background("red");
+
+        steps.push(Date.now());
+
+        if (steps.length > 10) {
+          steps.shift();
+        }
+        // get the difference between the time of each step
+        // remove times that are too long or too short
+        difference = diff(steps).filter((value) => value > 200 && value < 5000);
+
+        if (difference.length > 2) {
+          console.log(difference);
+          // console.log(`Recent step ${difference[difference.length - 1]}`);
+
+          averageStepSpeed = mean(difference);
+          // console.log(`Avg speed: ${averageStepSpeed}`);
+        }
       }
+
+      // rect shows how fast you're walking
+      let y = p5.map(averageStepSpeed, 0, 3000, 0, p5.windowHeight) ?? 10;
+      // console.log("🚀 ~ file: App.svelte ~ line 159 ~ sketch ~ height", y);
+
+      p5.fill("green");
+      p5.rect(0, y, p5.windowWidth, 20);
+
       // plotting the chart
       let now = new Date();
       chartRef.addDataPoint(now.getSeconds(), [magNoG[49], meanPeakHeight]);
@@ -135,69 +149,6 @@
       if (p5.frameCount > 20) {
         chartRef.removeDataPoint(0);
       }
-
-      //   accelerationZ = p5.accelerationZ.toFixed(2);
-      //   accelerationY = p5.accelerationY.toFixed(2);
-
-      //   if (
-      //     Math.abs(p5.accelerationZ - p5.pAccelerationZ) > move_threshold &&
-      //     Math.abs(p5.accelerationY - p5.pAccelerationY) >
-      //       move_forward_threshold &&
-      //     detectStepsp5
-      //   ) {
-      //     if (Date.now() - lastDebounceTime > debounceDelay) {
-      //       p5.fill("red");
-      //       p5.textSize(40);
-      //       p5.text("stepping", 100, 100);
-
-      //       // if we detect a step add its current time to an array
-      //       if (stepping) {
-      //         steps.push(Date.now());
-
-      //         // if a step was detected don't allow another until debounce time is reached
-      //         stepping = false;
-
-      //         // get the difference between the time of each step
-      //         // remove times that are too long or too short
-      //         difference = diff(steps).filter(
-      //           (value) => value > 200 && value < 5000
-      //         );
-
-      //         // if we have at least 5 steps in the array then average the time diff to get an average speed
-
-      //         // TODO: do some more testing to make sure it isn't picking up double triggers
-      //         // also figure out why it skips some steps
-      //         if (steps.length > 5) {
-      //           console.log(difference);
-      //           console.log(`Recent step ${difference[difference.length - 1]}`);
-      //           averageStepSpeed = average(difference);
-
-      //           // lighter colors for slower speed
-      //           if (averageStepSpeed) {
-      //             console.log(`Avg speed: ${averageStepSpeed}`);
-      //             if (averageStepSpeed < 750) {
-      //               p5.background("#0000AD");
-      //             } else if (averageStepSpeed > 750 && averageStepSpeed < 1000) {
-      //               p5.background("#1D9AFF");
-      //             } else if (averageStepSpeed > 1000) {
-      //               p5.background("#33C1FF");
-      //             } else if (averageStepSpeed > 2000) {
-      //               p5.background("#D1FFE0");
-      //             }
-      //           }
-      //         }
-      //       }
-
-      //       // make sure array is always 10 units long
-      //       if (steps.length > 10) {
-      //         steps.shift();
-      //       }
-      //     }
-      //   } else {
-      //     lastDebounceTime = Date.now();
-      //     p5.background(255);
-      //     stepping = true;
-      //   }
     };
   };
 
@@ -206,20 +157,35 @@
       return n - A[i];
     });
   }
-  function average(arr) {
-    const sum = arr.reduce((a, b) => a + b, 0);
-    const avg = sum / arr.length || 0;
 
-    return avg;
+  function requestAccess() {
+    // @ts-ignore
+    DeviceOrientationEvent.requestPermission()
+      .then((response, event) => {
+        if (response == "granted") {
+          permissionGranted = true;
+        } else {
+          permissionGranted = false;
+        }
+      })
+      .catch(console.error);
+
+    this.remove();
   }
 </script>
 
-{#if permissionGranted}
-  <Chart {data} type="line" bind:this={chartRef} />
-{/if}
+<LayoutGrid id="context">
+  {#if permissionGranted}
+    <Chart {data} type="line" bind:this={chartRef} />
+  {:else}
+    <!-- <Button on:click={requestAccess}>Allow sensors</Button> -->
+  {/if}
 
-<LayoutGrid>
-  <Cell>
+  {#if showButton}
+    <Button on:click={requestAccess}>Allow sensors</Button>
+  {/if}
+
+  <!-- <Cell>
     <Slider
       style="flex-grow: 1;"
       bind:value={debounceDelay}
@@ -228,7 +194,7 @@
     />
 
     Debounce delay: {debounceDelay}
-  </Cell>
+  </Cell> -->
 
   <Cell>
     <Slider
@@ -242,7 +208,7 @@
     up/down threshold: {move_threshold}
   </Cell>
 
-  <Cell>
+  <!-- <Cell>
     <Slider
       style="flex-grow: 1;"
       bind:value={move_forward_threshold}
@@ -253,9 +219,11 @@
     />
 
     Forward threshold: {move_forward_threshold}
-  </Cell>
+  </Cell> -->
 
   <!-- <Cell>Forward: {accelerationY}</Cell>
   <Cell>Up/Down: {accelerationZ}</Cell> -->
-  <Cell><P5 {sketch} /></Cell>
+  <!-- <Cell /> -->
 </LayoutGrid>
+
+<P5 {sketch} />
